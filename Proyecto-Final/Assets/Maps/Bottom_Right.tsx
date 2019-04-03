2905614773b2e753cef1262eb87e5bd241c366a4
Ruben <rubenosmani.dc@gmail.com>
2019-04-03 12:50:02 -0400

    Other changes

---
+++ Proyecto-Final/Assets/Maps/Bottom_Right.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Bottom_Right" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="../AlienTileSet/Sprites/Tiles/Bottom_Right.png" width="512" height="512"/>
+</tileset>
---
+++ Proyecto-Final/Assets/Maps/Gras_Middle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Gras_Middle" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="../AlienTileSet/Sprites/Tiles/Gras_Middle.png" width="512" height="512"/>
+</tileset>

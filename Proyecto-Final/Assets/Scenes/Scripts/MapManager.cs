cd7748d91e6c5305666277f7be88dab7c859b128
Ruben <rubenosmani.dc@gmail.com>
2019-04-04 00:13:00 -0400

    alk

---
+++ Proyecto-Final/Assets/Scenes/Scripts/MapManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    GameObject cloud1, cloud2, cloud3;
+    GameObject mountainfront, mountainback;
+    GameObject floor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

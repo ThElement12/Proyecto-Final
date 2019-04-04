cb709608e9b7ff2aaf1c88c8f6b2ae6551963504
Ruben <rubenosmani.dc@gmail.com>
2019-04-04 10:46:43 -0400

    mapManager.cs  huge update

--- Proyecto-Final/Assets/Scenes/Scripts/MapManager.cs
+++ Proyecto-Final/Assets/Scenes/Scripts/MapManager.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    GameObject cloud1, cloud2, cloud3;
-    GameObject mountainfront, mountainback;
-    GameObject floor;
+    public GameObject enemyGen, fightingWall, lastWall, firstWall, nullWall;
+    public GameObject floor, firstFloor, lastFloor;
+    public float floorsize;
+
+    Dictionary<char, GameObject> cellPrefabs;
+    GameObject _newCell;
+    XmlDocument level;
+
+    private void Awake()
+    {
+        cellPrefabs = new Dictionary<char, GameObject>
+        {
+            {'_', floor },
+            {'|', enemyGen },
+            {'-', firstFloor },
+            {'/', lastFloor },
+            {'*', fightingWall },
+            {';', lastWall },
+            {'f', firstWall },
+            {' ', nullWall }
+        };
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        level = new XmlDocument();
+        level.LoadXml(Resources.Load<TextAsset>("level1").text);
+        LoadMap();
         
     }
-
+    private void LoadMap()
+    {
+        int j, i = 0;
+        foreach(XmlNode actualRow in level.SelectNodes("//Level/Map/Row"))
+        {
+            j = 0;
+            i--;
+            foreach(char actualCell in actualRow.InnerText)
+            {
+                _newCell = Instantiate(cellPrefabs[actualCell], new Vector3(j, i, cellPrefabs[actualCell].transform.position.z), Quaternion.identity);
+                _newCell.transform.Translate(new Vector3(floorsize*j, 0));
+                j++;
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {

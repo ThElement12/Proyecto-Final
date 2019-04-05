08900fcb2dc597160b088fb11291f22152128339
Ruben <rubenosmani.dc@gmail.com>
2019-04-04 21:51:26 -0400

    jump,fightingwalls now works

--- Proyecto-Final/Assets/Scenes/Scripts/CharacterMovement.cs
+++ Proyecto-Final/Assets/Scenes/Scripts/CharacterMovement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public bool noCollision = false;
     Vector3 aceleration = new Vector3(0, Physics.gravity.y);
+    Vector3 jumpforce;
     Vector3 normalSpeed = Vector3.zero;
     bool isOnPlatform = false;
     Vector3 position;
+    int jump = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +22,22 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        position.x = Input.GetAxis("Horizontal") * normalSpeed.x;
+        if(!noCollision)
+            position.x = Input.GetAxis("Horizontal") * normalSpeed.x;
+
         if (!isOnPlatform)
         {
-            position.y += normalSpeed.y * Time.deltaTime + (aceleration.y * Mathf.Pow(Time.deltaTime, 2)) / 2;
-            normalSpeed.y = normalSpeed.y + aceleration.y * Time.deltaTime;
+            position.y = normalSpeed.y * Time.deltaTime + aceleration.y * (Mathf.Pow(Time.deltaTime, 2) / 2);
+            normalSpeed.y += aceleration.y * Time.deltaTime;
         }
         position *= Time.deltaTime;
+        if (Input.GetButtonDown("Jump") && jump == 1)
+        {
+            jump = 0;
+            isOnPlatform = false;
+            normalSpeed.y = 170;
+            aceleration.y = Physics.gravity.y;
+        }
         transform.Translate(position);
     }
 
@@ -36,6 +48,7 @@ public class CharacterMovement : MonoBehaviour
             normalSpeed.y = 0;
             aceleration.y = 0;
             isOnPlatform = true;
+            jump = 1;
         }
     }
 }
---
+++ Proyecto-Final/Assets/Scenes/Scripts/fightingWall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fightingWall : MonoBehaviour
+{
+    public static bool inFight = false;
+    public bool fightnotOver = true;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(tag == "wallEnter" && fightnotOver)
+        {
+            if(other.gameObject.tag == "Player")
+            {
+                inFight = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (inFight)
+        {
+            other.gameObject.GetComponent<CharacterMovement>().noCollision = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (tag == "wallEnter" && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            other.gameObject.GetComponent<CharacterMovement>().noCollision = false;
+        }
+
+        else if (tag == "wallExit" && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            other.gameObject.GetComponent<CharacterMovement>().noCollision = false;
+        }
+    }
+}

133b622bbf66728188c27c8cd06408a04621a1d6
Joseph <josephcontraelmenor12@gmail.com>
2019-04-11 17:00:25 -0400

    as

---
+++ Proyecto-Final/Assets/Scripts/Class/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Class
+{
+    [DataContract]
+    public class PlayerStats
+    {
+        [DataMember]
+        public bool Pass { get; set; }
+        [DataMember]
+        public string UserName { get; set; }
+        [DataMember]
+        public int NivelesLogrados { get; set; }
+        [DataMember]
+        public List<Items> Inventario { get; set; }
+    }
+}
---
+++ Proyecto-Final/Assets/Scripts/ControlNivel/SaveEstateManager.cs
@@ -0,0 +1,56 @@
+﻿using Assets.Scripts.Class;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class SaveEstateManager : MonoBehaviour
+{
+    static string RutaXML;
+    public static PlayerStats CurrentGame;
+    public static bool Guardado = false;
+    public static bool Cargado = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentGame = new PlayerStats();
+        RutaXML = Application.persistentDataPath + "/" + ControlJuego.UserName;
+        CurrentGame.UserName = ControlJuego.UserName;
+        CurrentGame.NivelesLogrados = ControlJuego.NivelesLogrados;
+        CurrentGame.Inventario = ControlJuego.Inventario;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    public static void SaveState()
+    {
+        DataContractSerializer dcSerializer = new DataContractSerializer(typeof(PlayerStats));
+        using(FileStream fstream = new FileStream(RutaXML, FileMode.Create))
+        {
+            dcSerializer.WriteObject(fstream,CurrentGame);
+        }
+    }
+    public static void LoadState()
+    {
+        DataContractSerializer dcSerializer = new DataContractSerializer(typeof(PlayerStats));
+        try
+        {
+            using(FileStream fstream = new FileStream(RutaXML, FileMode.Open))
+            {
+                CurrentGame = (PlayerStats)dcSerializer.ReadObject(fstream);
+            }
+        }
+        catch (FileNotFoundException)
+        {
+
+            throw;
+        }
+
+    }
+  
+}

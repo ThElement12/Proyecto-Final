a632492281581a7a32aa7afb75f952f9f4c758a2
Ruben <rubenosmani.dc@gmail.com>
2019-04-12 09:51:54 -0400

    ,nj

--- Proyecto-Final/Assets/Scripts/MenuScripts/AudioManager.cs
+++ Proyecto-Final/Assets/Scripts/MenuScripts/AudioManager.cs
@@ -7,6 +7,30 @@ public class AudioManager : MonoBehaviour
     public AudioSource HoverSound, ClickSound, Fondo;
     public static bool play = true;
 
+    void Awake()
+    {
+        /*if(play)
+            GetComponent<AudioSource>().playOnAwake = true;*/
+    }
+
+    private void Update()
+    {
+        if (!Fondo.isPlaying && play)
+        {
+            Fondo.Play();
+        }
+        else if(Fondo.isPlaying && !play)
+        {
+            Fondo.Stop();
+        }
+    }
+    public void MusicOff()
+    {
+        if (!play)
+            Fondo.Stop();
+        else
+            Fondo.Play();
+    }
 
     public void PlayHoverSound()
     {

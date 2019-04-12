24b9ac16e734e1419cd0f2bee609b5b4cc6e7874
Ruben <rubenosmani.dc@gmail.com>
2019-04-11 23:52:51 -0400

    alv cuanta mierda

--- Proyecto-Final/Assets/Scenes/ParallaxMenu/ParallaxMenu.cs
+++ Proyecto-Final/Assets/Scenes/ParallaxMenu/ParallaxMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ParallaxMenu : MonoBehaviour
 {
-    //public GameObject myChild;
+    public GameObject myChild;
     public float speed;
     Vector3 position = Vector3.zero;
     
@@ -18,10 +18,37 @@ public class ParallaxMenu : MonoBehaviour
     void Update()
     {
         transform.Translate(position);
-        if(transform.position.x <= -myChild.transform.position.x)
+        switch (tag)
         {
-            Instantiate(myChild);
-            Destroy(gameObject);
+            case "mountain1":
+                if (transform.position.x <= -26.6f)
+                {
+                    Instantiate(myChild, new Vector3(26.6f, myChild.transform.position.y, myChild.transform.position.z), Quaternion.identity);
+                    Destroy(gameObject);
+                }
+                break;
+            case "mountain2":
+                if (transform.position.x <= -34.71f)
+                {
+                    Instantiate(myChild, new Vector3(34.71f, myChild.transform.position.y, myChild.transform.position.z), Quaternion.identity);
+                    Destroy(gameObject);
+                }
+                break;
+            case "cloud":
+                if (transform.position.x <= -25.6f)
+                {
+                    Instantiate(myChild, new Vector3(25.6f, myChild.transform.position.y, myChild.transform.position.z), Quaternion.identity);
+                    Destroy(gameObject);
+                }
+                break;
+            case "Platform":
+                if (transform.position.x <= -33f)
+                {
+                    Instantiate(myChild, new Vector3(9f, myChild.transform.position.y, myChild.transform.position.z), Quaternion.identity);
+                    Destroy(gameObject);
+                }
+                break;
         }
+        
     }
 }
